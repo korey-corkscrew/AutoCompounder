bedb37638b04c26b5eb3bfd3cbf57d37074f5a17
Korey Corkscrew <cameronbiniamow@Camerons-MacBook-Pro.local>
2022-04-30 13:55:57 -0500

    working/testing

--- contracts/AutoCompounder.sol
+++ contracts/AutoCompounder.sol
@@ -24,17 +24,16 @@ contract AutoCompounder is Ownable {
     mapping(address => Compound) public compoundInfo;
     mapping(address => bytes32) public tasks;
     
-    IPokeMe public constant gelato = IPokeMe(0x527a819db1eb0e34426297b03bae11F2f8B3A19E);
-    IGasTank public constant gasTank = IGasTank(0x8090e4C077E0372B38E5Eed5e71FA8a2F79171bB);
-    IMasterChefV2 public constant masterChef = IMasterChefV2(0x6F78679615D0dd9a87070b37e7F2f49Ee0E1FA98);
-    uint256 public constant rewardPoolId = 0;
-    IERC20 public constant rewardToken = IERC20(0xfd7378e0Ebe9d2636317C4B9F472C3DF83b12917);
-
     // Gelato address that receives the gas fee after execution
     address payable public constant gelatoPayee = payable(0x7598e84B2E114AB62CAB288CE5f7d5f6bad35BbA);
-
-    // Native token
     address public constant ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+    IPokeMe public constant gelato = IPokeMe(0x527a819db1eb0e34426297b03bae11F2f8B3A19E);
+    
+    IGasTank public immutable gasTank;// = IGasTank(0x8090e4C077E0372B38E5Eed5e71FA8a2F79171bB);
+    IMasterChefV2 public immutable masterChef;// = IMasterChefV2(0x6F78679615D0dd9a87070b37e7F2f49Ee0E1FA98);
+    uint256 public immutable rewardPoolId;// = 0;
+    IERC20 public immutable rewardToken;// = IERC20(0xfd7378e0Ebe9d2636317C4B9F472C3DF83b12917);
+
 
     modifier hasCompounder() {
         require(address(compounders[msg.sender]) != address(0), "CornFi Auto Compounder: User not created");
@@ -44,6 +43,14 @@ contract AutoCompounder is Ownable {
     // --------------------------------------------------------------------------------
     // --------------------------------------------------------------------------------
     
+    constructor(IGasTank _gasTank, IMasterChefV2 _mc, uint256 _rewardPool, IERC20 _rewardToken) {
+        gasTank = _gasTank;
+        masterChef = _mc;
+        rewardPoolId = _rewardPool;
+        rewardToken = _rewardToken;
+    }
+
+    // --------------------------------------------------------------------------------
 
     function pendingRewardPools(address _user) public view returns (uint256[] memory) {
         address comp = address(compounders[_user]);
@@ -140,8 +147,9 @@ contract AutoCompounder is Ownable {
 
     // --------------------------------------------------------------------------------
 
-    function createAutoCompounder() external {
+    function createAutoCompounder() external returns (address) {
         require(address(compounders[msg.sender]) == address(0), "CornFi Auto Compounder: User already created");
         compounders[msg.sender] = new Compounder(masterChef, rewardPoolId, rewardToken, msg.sender);
+        return(address(compounders[msg.sender]));
     }
 }
--- scripts/deploy/deployAutoCompounder.js
+++ scripts/deploy/deployAutoCompounder.js
@@ -16,16 +16,95 @@ async function main() {
   // manually to make sure everything is compiled
   // constructor args
 
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+  const account = new ethers.Wallet(process.env.PRIVATE_KEY);
+  const signer = account.connect(provider);
 
-  // We get the contract to deploy
+
+  const GasTank = await ethers.getContractFactory("GasTank");
+  // const gasTank = await GasTank.deploy();
+  const gt = GasTank.attach("0xFc5cFd9616720d1b17c25D13F341F8189DEA740c");
+  // const gt = await gasTank.deployed();
+  // console.log(`
+  //   Gas Tank deployed at ${gt.address}
+  // `);
+
+  // ------------------------------------------------------------
+
+  const BobToken = await ethers.getContractFactory("BobToken");
+  // const bobToken = await BobToken.deploy(signer.address);
+  const bob = BobToken.attach("0xE5AB747c4BF94BDa6a80400f96C6cBB096607651");
+  // const bob = await bobToken.deployed();
+  // console.log(`
+  //   Test token depoloyed at ${bob.address}
+  // `);
+
+  // // ------------------------------------------------------------
+
+  // const cobPerBlock = ethers.utils.parseUnits("2.24", "ether");
+  // const startblock = BigNumber.from(27745197);
+  const MasterChefV2 = await ethers.getContractFactory("MasterChefV2");
+  // const masterchef = await MasterChefV2.deploy(bob.address, signer.address, signer.address, cobPerBlock, startblock);
+  const mc = MasterChefV2.attach("0xe11d9D3b273c885dBA123CDB71526f747fBA40F9");
+  // const mc = await masterchef.deployed();
+  // console.log(`
+  //   MasterChef depoloyed at ${mc.address}
+  // `);
+
+  // await mc.add(50, bob.address, 10, false);
+  // console.log(`
+  //   Pool: ${bob.address} added to MasterChef
+  // `);
+
+  // await bob.transferOwnership(mc.address);
+  // console.log(`
+  //   Token ownership transfer
+  // `);  
+
+  // // ------------------------------------------------------------
+
+  const rewardPool = 0;
   const AutoCompounder = await ethers.getContractFactory("AutoCompounder");
-  const autoCompounder = AutoCompounder.attach("0xeE4Aa63A0fD6853A460B9b56EbEd74930DFf763c");
-  // await autoCompounder.createAutoCompounder();
-  await autoCompounder.deposit(0, ethers.utils.parseUnits('100', "ether"));
+  // const autoCompounder = await AutoCompounder.deploy(gt.address, mc.address, rewardPool, bob.address);
+  const ac = AutoCompounder.attach("0x7F47C4684Fc73F0427b1f497e244cb693CaF5eA6");
+  // const ac = await autoCompounder.deployed();
+  // console.log(`
+  //   Auto Compounder depoloyed at ${ac.address}
+  // `);
 
-  // const autoCompounder = await AutoCompounder.deploy();
-  
-  // await autoCompounder.deployed();
+  // const compounder = await ac.createAutoCompounder();
+  // console.log(`
+  //   Compounder created at ${compounder}
+  // `);
+
+  const compounder = await ac.compounders(signer.address);
+
+  // await ac.startAutoCompound(300);
+  // console.log(`
+  //   Auto compounder started
+  // `);
+
+  await ac.stopAutoCompound();
+
+  // await gt.depositGas(signer.address, {'value': ethers.utils.parseUnits('0.5', "ether")});
+  // console.log(`
+  //   Gas deposit.
+  // `);
+
+  // await gt.addPayee(ac.address)
+  // console.log(`
+  //   Payee: ${ac.address} approved for payement with Gas Tank: ${gt.address}
+  // `);
+
+  // await bob.approve(compounder, ethers.utils.parseUnits('10000000', "ether"));
+  // console.log(`
+  //   Token ${bob.address} approved for trade on ${compounder}
+  // `);
+
+  // await ac.deposit(0, ethers.utils.parseUnits('100', "ether"));
+  // console.log(`
+  //   Auto compound desposit
+  // `);
 }
 
 
